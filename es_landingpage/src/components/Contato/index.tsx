5f0648a4c0e659e9013ca3f4f54240299343f802
Matheus Alefe <matheus.orvati@hotmail.com>
2022-09-13 21:57:36 -0300

    Remover importação não utilizada

--- es_landingpage/src/components/Contato/index.tsx
+++ es_landingpage/src/components/Contato/index.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes, ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 import { Container } from "./styles";
 
 interface ProdutoProps extends ButtonHTMLAttributes<HTMLButtonElement> {
